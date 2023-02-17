8e765ac19e59e3a0745dfcec46281cdef8fcfba1
unknown <tomygarnero9429@gmail.com>
2023-02-17 18:12:11 -0300

    Install and Activate a Service Worker

---
+++ sw.js
@@ -0,0 +1,14 @@
+
+// Cuando se instala el Service Worker
+self.addEventListener("install", e => {
+    console.log("Instalado el Service Worker");
+
+    console.log(e);
+})
+
+// Activar el Service Worker
+self.addEventListener("activate", e => {
+    console.log("Service Worker activado");
+
+    console.log(e);
+})
