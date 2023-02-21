3a0b4d425fc42ee3919e7fccb913997ef470832d
unknown <tomygarnero9429@gmail.com>
2023-02-21 12:07:02 -0300

    Change some files

--- sw.js
+++ sw.js
@@ -10,23 +10,24 @@ const archivos = [
     "/js/apv.js"
 ];
 
-// Cuando se instala el Service Worker
+// Cuando se instala el Service Worker, no se puede utilizar window, por lo tanto se utiliza self
 self.addEventListener("install", e => {
     console.log("Instalado el Service Worker");
 
     e.waitUntil(
         caches.open(nombreCache)
             .then(cache => {
-                console.log("Cacheando");
+                console.log("Cacheando...");
                 cache.addAll(archivos);
             })
-    ) 
+    );
 })
 
 // Activar el Service Worker
 self.addEventListener("activate", e => {
     console.log("Service Worker activado");
 
+    // Actualizar la PWA
     e.waitUntil(
         caches.keys()
             .then(keys => {
@@ -46,8 +47,20 @@ self.addEventListener("fetch", e => {
     e.respondWith(
         caches.match(e.request)
             .then(respuestaCache => {
-                return respuestaCache;
+                return respuestaCache || fetch(e.request);
             })
             .catch(() => caches.match("/error.html"))
     )
 })
+
+
+// Al tener este fetch, un nombre de app, y una página de inicio, ya podremos agregar nuestra PWA a la página de inicio...
+
+
+// clICK EN LOS 3 PUNTOS
+
+//  More Tools -> Remote Devices y escribir.
+
+// http://127.0.0.1:5500/index.html
+
+// Presionar Open.
